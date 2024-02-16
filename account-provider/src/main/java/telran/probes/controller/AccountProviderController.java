5e75c57de4376a3af73d94e3caf8343d0a473ec9
Alexey Drumlevich <l-gc@yandex.ru>
2024-02-16 21:14:50 +0200

    add account-provider

---
+++ account-provider/src/main/java/telran/probes/controller/AccountProviderController.java
@@ -0,0 +1,21 @@
+package telran.probes.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import telran.probes.dto.AccountDto;
+import telran.probes.service.AccountProviderService;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class AccountProviderController {
+	final AccountProviderService accountingService;	
+	@GetMapping("accounts/{email}")
+	AccountDto getAccount(@PathVariable String email) {
+		return accountingService.getAccount(email);
+	}
+}
